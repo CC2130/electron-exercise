496a80e037a403f7e680e1da79a2f4b8d23ac7f4
Jia Chao <jiachao2130@126.com>
2022-04-21 14:10:08 +0800

    01-hello-electron

---
+++ examples/01-hello/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+// 首次启动或恢复到 activate 时创建窗口
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  //if (process.platform !== 'drawin') {
+  //  app.quit()
+  //}
+})
---
+++ examples/01-hello/preload.js
@@ -0,0 +1,11 @@
+// DOM 载入时即完成替换
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+})
